9f63a4a41709459d1db46d27e1de82975a9b6d30
dmytro.popov <dmytro.popov@711media.de>
2022-04-21 17:22:57 +0300

    meetup app with nextjs code basics repeat

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import Head from 'next/head';
-import Layout from '@/components/Layout';
-
-import '@/styles/globals.scss';
+import Layout from '../components/layout/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <meta name="description" content="Events" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );

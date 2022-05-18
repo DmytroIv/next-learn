4c785f12e7d78b8720e1ab0df2ed8a456189999f
DmytroIv <dmytropopovkh@gmail.com>
2022-05-18 10:42:25 +0300

    meta tags & OpenGraph meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import Head from "next/head";
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>My app</title>
       <meta name="description" content="My app" />
+      <meta property="og:url" content={ process.env.NEXT_PUBLIC_DOMAIN + router.asPath } />
+      <meta property="og:locale" content="en_US" />
     </Head>
     <Component { ...pageProps } />
   </>;
